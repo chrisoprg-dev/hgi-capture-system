4e1985079fcaea6e859be94a3f502781c415e1a0
chrisoprg-dev <chrisoprg@gmail.com>
2026-03-23 00:02:43 -0500

    MCP: edit api/run.js

--- api/run.js
+++ api/run.js
@@ -20,6 +20,8 @@ export default async function handler(req, res) {
       if (target === 'generate-doc') url = url + '?opp=' + encodeURIComponent(oppId);
     } else if (target === 'rebuild-apify' && oppId) {
       url = url + '?actor=' + encodeURIComponent(oppId);
+    } else if ((target === 'red-team' || target === 'proposal-loop') && oppId === 'force') {
+      url = url + '?force=true';
     } else if (target === 'organism-work' || target === 'organism-think') {
       opts.body = JSON.stringify({ trigger: 'mcp_manual' });
     } else {
